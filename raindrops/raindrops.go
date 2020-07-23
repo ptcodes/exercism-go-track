a57fcb19210b298b418cb9bee21e34c276be0f3e
Pavel Timofeev <pav.timofeev@gmail.com>
2020-07-23 11:56:54 -0400

    add raindrops

---
+++ raindrops/raindrops.go
@@ -0,0 +1,19 @@
+package raindrops
+
+import "strconv"
+
+func Convert(num int) (result string) {
+	if num%3 == 0 {
+		result += "Pling"
+	}
+	if num%5 == 0 {
+		result += "Plang"
+	}
+	if num%7 == 0 {
+		result += "Plong"
+	}
+	if len(result) == 0 {
+		result += strconv.Itoa(num)
+	}
+	return
+}
