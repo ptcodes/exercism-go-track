9e5f5cc75bdaadcde0e83fb3472a69607a717a80
Pavel Timofeev <pav.timofeev@gmail.com>
2020-07-25 21:32:24 -0400

    add etl

---
+++ etl/etl.go
@@ -0,0 +1,13 @@
+package etl
+
+import "strings"
+
+func Transform(input map[int][]string) map[string]int {
+	output := make(map[string]int)
+	for score, letters := range input {
+		for _, letter := range letters {
+			output[strings.ToLower(letter)] = score
+		}
+	}
+	return output
+}
