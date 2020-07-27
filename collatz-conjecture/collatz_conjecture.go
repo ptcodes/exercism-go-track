0ba02b37f74ca8a42ceac67d088b7d4ea9f7298e
Pavel Timofeev <pav.timofeev@gmail.com>
2020-07-27 16:41:53 -0400

    add collatz-conjecture

---
+++ collatz-conjecture/collatz_conjecture.go
@@ -0,0 +1,22 @@
+package collatzconjecture
+
+import (
+	"errors"
+)
+
+func CollatzConjecture(n int) (int, error) {
+	if n <= 0 {
+		return 0, errors.New("must be positive")
+	}
+
+	steps := 0
+	for n > 1 {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+		steps++
+	}
+	return steps, nil
+}
